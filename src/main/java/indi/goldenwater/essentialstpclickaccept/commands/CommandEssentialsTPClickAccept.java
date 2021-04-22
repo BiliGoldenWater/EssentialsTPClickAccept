def2bebf147821fe86101067af900fa4484c3366
BiliGoldenWater <2439577268@qq.com>
2021-04-23 02:56:12 +0800

    finish add I18n system.

--- src/main/java/indi/goldenwater/essentialstpclickaccept/commands/CommandEssentialsTPClickAccept.java
+++ src/main/java/indi/goldenwater/essentialstpclickaccept/commands/CommandEssentialsTPClickAccept.java
@@ -1,26 +1,125 @@
 package indi.goldenwater.essentialstpclickaccept.commands;
 
+import indi.goldenwater.essentialstpclickaccept.EssentialsTPClickAccept;
+import indi.goldenwater.essentialstpclickaccept.utils.I18nManager;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.Configuration;
 
-import java.util.Arrays;
+import static indi.goldenwater.essentialstpclickaccept.utils.CheckPermissions.hasPermissions_Tips;
 
 public class CommandEssentialsTPClickAccept {
-    private static JavaPlugin plugin;
+    private static EssentialsTPClickAccept plugin;
+    private static Configuration config;
+    private static I18nManager i18n;
+    private static String lang;
+    private static String noPerm;
 
-    public static void register(String mainCommand, JavaPlugin pluginInstance) {
+    public static void register(String mainCommand, EssentialsTPClickAccept pluginInstance) {
         plugin = pluginInstance;
-        CommandHandlerManager commandHandlerManager = new CommandHandlerManager(mainCommand);
-        commandHandlerManager.register();
+        String permHead = "essentialstpclickaccept.command.";
+        CommandHandlerManager handlerManager = new CommandHandlerManager(mainCommand);
+        handlerManager.register();
 
-        commandHandlerManager.setHandler("__main__", (commandSender, args) -> {
-            commandSender.sendMessage(Arrays.toString(args));
+        handlerManager.registerHandler("_ _main_ _", (sender, command, args) -> {
+            update();
+            if (!hasPermissions_Tips(sender, permHead + command, noPerm)) return true;
+            HelpMessageManager.sendHelpMessage(sender, HelpMessageManager.HelpLvl.full);
             return true;
         });
 
-        commandHandlerManager.setHandler("help", (commandSender, args) -> {
-            commandSender.sendMessage(Arrays.toString(args));
+        handlerManager.registerHandler("help", (sender, command, args) -> {
+            update();
+            if (!hasPermissions_Tips(sender, permHead + command, noPerm)) return true;
+
+            HelpMessageManager.sendHelpMessage(sender, HelpMessageManager.HelpLvl.full);
+
             return true;
         });
+
+        handlerManager.registerHandler("lang", (sender, command, args) -> {
+            update();
+            if (!hasPermissions_Tips(sender, permHead + command, noPerm)) return true;
+            String targetLanguage = null;
+
+            if (args.length == 0) {
+                HelpMessageManager.sendHelpMessage(sender, HelpMessageManager.HelpLvl.lang); // 发送命令用法
+            } else {
+                targetLanguage = args[0];
+            }
+
+            i18n.setLanguage(plugin,
+                    sender,
+                    lang,
+                    targetLanguage,
+                    "lang",
+                    "cmd_lang_list",
+                    "cmd_lang_successSet",
+                    "cmd_lang_setFail",
+                    "cmd_lang_setFail_doesNotExist");
+
+            return true;
+        });
+
+        handlerManager.registerHandler("resetlang", (sender, command, args) -> {
+            update();
+            if (!hasPermissions_Tips(sender, permHead + command, noPerm)) return true;
+
+            plugin.getI18n()
+                    .releaseDefaultLangFile(plugin, "lang", "langList.json", true);
+            sender.sendMessage(i18n.getL10n(lang, "cmd_resetLang_resetFinish"));
+            return true;
+        });
+
+        handlerManager.registerHandler("reload", (sender, command, args) -> {
+            update();
+            if (!hasPermissions_Tips(sender, permHead + command, noPerm)) return true;
+            plugin.getI18n().reload();
+            plugin.reloadConfig();
+
+            update();
+            sender.sendMessage(i18n.getL10n(lang, "cmd_reload_success"));
+            return true;
+        });
+    }
+
+    public static void update() {
+        config = plugin.getConfig();
+        i18n = plugin.getI18n();
+        lang = config.getString("lang");
+        noPerm = i18n.getL10n(lang, "error_noPerm");
+    }
+
+    public static class HelpMessageManager {
+        public static void sendHelpMessage(CommandSender sender, int helpLvl) {
+            sender.sendMessage("EssentialsTPClickAccept by.Golden_Water");
+            switch (helpLvl) {
+                case HelpLvl.full:
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_help"));
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_lang"));
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_resetLang"));
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_reload"));
+                    break;
+                case HelpLvl.help:
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_help"));
+                    break;
+                case HelpLvl.lang:
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_lang"));
+                    break;
+                case HelpLvl.resetlang:
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_resetLang"));
+                    break;
+                case HelpLvl.reload:
+                    sender.sendMessage(i18n.getL10n(lang, "help_usage_reload"));
+                    break;
+            }
+        }
+
+        public static class HelpLvl {
+            public static final int full = 0;
+            public static final int help = 1;
+            public static final int lang = 2;
+            public static final int resetlang = 3;
+            public static final int reload = 4;
+        }
     }
 }
--- src/main/java/indi/goldenwater/essentialstpclickaccept/commands/CommandHandlerManager.java
+++ src/main/java/indi/goldenwater/essentialstpclickaccept/commands/CommandHandlerManager.java
@@ -5,35 +5,38 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CommandHandlerManager {
-    private final PluginCommand command;
     private final Map<String, CommandHandler> commandHandlers = new HashMap<>();
+    private final List<String> subcommands = new ArrayList<>();
+    private final String mainCommandName;
+    private final PluginCommand command;
+    private String mainCommandReplacer = "_ _main_ _";
 
     CommandHandlerManager(String mainCommand) {
+        mainCommandName = mainCommand;
         command = Bukkit.getPluginCommand(mainCommand);
     }
 
     public void register() {
         command.setExecutor((sender, command, s, args) -> {
             if (args.length == 0) { // 当无指令参数时
-                CommandHandler handler = commandHandlers.get("__main__"); // 获取名为__main__的指令处理类
+                CommandHandler handler = commandHandlers.get(mainCommandReplacer); // 获取名为__main__的指令处理类
                 return handler != null &&
-                        handler.handle(sender, null); // 如果为空则返回false 如果有则以空参数运行处理器
+                        handler.handle(sender, mainCommandName, new String[]{}); // 如果为空则返回false 如果有则以空参数运行处理器
 
             } else { // 当有指令参数时
-                CommandHandler handler = commandHandlers.get(args[1]); // 获取指令处理
+                CommandHandler handler = commandHandlers.get(args[0].toLowerCase()); // 获取指令处理
 
                 if (handler == null) {
-                    handler = commandHandlers.get("__main__"); // 获取名为__main__的指令处理器
+                    handler = commandHandlers.get(mainCommandReplacer); // 获取名为__main__的指令处理器
                     return handler != null &&
-                            handler.handle(sender, args); // 如果为空则返回false 如果有则带参数运行处理器
+                            handler.handle(sender, mainCommandName, args); // 如果为空则返回false 如果有则带参数运行处理器
                 }
 
                 return handler.handle(sender,
+                        args[0].toLowerCase(),
                         Arrays.asList(args)
                                 .subList(1, args.length)
                                 .toArray(new String[]{})); // 带参数运行处理
@@ -41,17 +44,39 @@ public class CommandHandlerManager {
         });
     }
 
-    public void setHandler(String subCommand, CommandHandler commandHandler) {
+    public void registerHandler(String subCommand, CommandHandler commandHandler) {
         if (subCommand != null && commandHandler != null) {
-            commandHandlers.put(subCommand, commandHandler);
+            commandHandlers.put(subCommand.toLowerCase(), commandHandler);
+            subcommands.add(subCommand.toLowerCase());
         }
     }
-    
+
+    public void setMainCommandReplacer(String replacer) {
+        mainCommandReplacer = replacer;
+    }
+
+    public void unregisterHandler(String subCommand) {
+        commandHandlers.remove(subCommand.toLowerCase());
+        subcommands.remove(subCommand.toLowerCase());
+    }
+
     public Command getCommand() {
         return command;
     }
 
+    public String getMainCommandReplacer() {
+        return mainCommandReplacer;
+    }
+
+    public Map<String, CommandHandler> getCommandHandlers() {
+        return commandHandlers;
+    }
+
+    public List<String> getSubcommands() {
+        return subcommands;
+    }
+
     public interface CommandHandler {
-        boolean handle(CommandSender commandSender, String[] args);
+        boolean handle(CommandSender commandSender, String commandName, String[] args);
     }
 }
